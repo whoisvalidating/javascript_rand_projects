17108749bc574df0d56650174d66b5819ad787d6
Whoisvalidating <115468637+whoisvalidating@users.noreply.github.com>
2023-10-22 18:45:29 -0500

    finished password gen - added 12 option some css javascript finished

--- password-gen/index.js
+++ password-gen/index.js
@@ -1,10 +1,15 @@
 let newPassEl = document.querySelector("#newpass-el")
-let options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "0", "@", "$", "&", "*", "J", "R"];
-//20 options
-console.log(options.length);
+let options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "@", "!", "$", "#", "&", "A", "R", "B", "U", "I", "V", "Q"]
+
+
 function start() {
     generate();
 }
+function start12() {
+        generate12();
+    }
+
+    
 function generate() {
     let newPass = "";
 
@@ -16,7 +21,18 @@ function generate() {
     newPassEl.textContent = newPass;
 }
 
+function generate12() {
+    let newPass = "";
+
+    for (let i = 0; i < 12; i++) {
+        if (i < 12) {
+            newPass += randomPiece()
+        }
+    }
+    newPassEl.textContent = newPass;
+}
+
 function randomPiece() {
-    let random = options[Math.floor(Math.random() * 23)];
+    let random = options[Math.floor(Math.random() * 48)];
     return random;
 }
