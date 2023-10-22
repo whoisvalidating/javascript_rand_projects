3ae5ad5b0bb89ad5f37a9a89c2be7b551241c81c
WhoIsValidating <115468637+whoisvalidating@users.noreply.github.com>
2023-10-21 23:54:15 -0500

    random card function added, new card button eligibility added. basic rules for j-A and bust condition.

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,18 +1,56 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
-let message = ""
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
+
+function getRandomCard() {
+    let value = Math.floor(Math.random() * 13) + 1;
+    if (value > 10) {
+        return 10;
+    } else if (value === 1) {
+        return 11;
+    } else {
+        return value
+    }
+}
 
 function startGame() {
-if (sum <= 20) {
-    message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-    message = "wohoo! you've got blackjack!";
-} else {
-    message = "you're out of the game my friend. Please try again";
-    isAlive = false;
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    renderGame();
 }
-console.log(message);
+
+function renderGame() {
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
+
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game my friend. Please try again";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
 };
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
+}
+}
