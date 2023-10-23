1a132a1185799e1b4c7347724d189a1299c15754
WhoIsValidating <115468637+whoisvalidating@users.noreply.github.com>
2023-10-23 00:21:47 -0500

    fixed naming on some assets basic bookmark/notes chrome extension added basic css,html for notes app complete java for basic functions complete --need local storage chrome://extensions/

---
+++ chrome-extension/index.js
@@ -0,0 +1,18 @@
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#Sul-el");
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    renderLeads();
+});
+
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
+    };
+    ulEl.innerHTML = listItems  
+};
